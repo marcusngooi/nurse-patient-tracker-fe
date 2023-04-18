17da59283df1d2b3a15526b36f74c133ae652392
Grant <grant.macmillan94@gmail.com>
2023-04-18 16:32:58 -0400

    Game page and Home page

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,9 +16,24 @@ function Game()
             preserveDrawingBuffer: true,
           },
       });
-    
+
+      const styles = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f0f0f0',
+        height: '100%', // Set the height of the container to 100%
+      };
+      
       return (
-        <Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <h1>Fitness Tracker Game</h1>
+            <div style={styles}>
+              <Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
+            </div>
+          </div>
+        </div>
       );
 }
 
