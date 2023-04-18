91b3c25a13d4fb4852a335f75f29cb6ccbe9a1d2
Grant <grant.macmillan94@gmail.com>
2023-04-18 16:16:18 -0400

    Home and index

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
-// Lab 3 Exercise 1
-// Author:      Marcus Ngooi (301147411)
+// COMP308-402 Group Project
+// Authors:     Marcus Ngooi (301147411)
 //              Ikamjot Hundal (301134374)
-// Description: Home's Page
+//              Ben Coombes
+//              Grant Macmillan (301129935)
+//              Gabriel Dias Tinoco
+//              Tatsiana Ptushko (301182173)
+// Description: Home Page
 import React from 'react';
 
 function Home(props)
@@ -9,9 +13,15 @@ function Home(props)
     return (
         <div>
             <h2>COMP308-402: Emerging Technologies</h2>
-            <h2>Assignment 3</h2>
-            <p>This MERN full-stack and GraphQL application allows an admin user to add students; students to enroll/drop courses; to view all students, all courses, students per course, and courses per student.
+            <h2>Group Project</h2>
+            <h4>Developing Apps Using Emerging Web Technologies</h4>
+            <p>This MERN full-stack and GraphQL application allows users to login and register as a nurse or a patient.
+                 Nurses can enter vital signs, send daily motivational tips, and generate a list of possible medical conditions based on the symptoms entered by the patient.
+                    Patients can enter their symptoms, and access a fitness game page, and send an emergency alert to first responders.
             </p>
+            <br />
+            <br />
+            <p>This application was created by: Marcus Ngooi, Ikamjot Hundal, Ben Coombes, Grant Macmillan, Tatsiana Ptushko and Gabriel Dias Tinoco </p>
         </div>
     );
 }
